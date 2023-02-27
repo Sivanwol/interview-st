cfd3e47234093c8cfdaf204102f8e4d9e86d6d6a
sivan wolberg <sivan@wolberg.pro>
2023-02-27 21:20:07 +0200

    chore: bit of staff

--- app/routes/tables/index.tsx
+++ app/routes/tables/index.tsx
@@ -1,11 +1,18 @@
 import { Card, CardBody } from "@chakra-ui/card";
-import { Text } from "@chakra-ui/react";
+import { Grid, GridItem, Text } from "@chakra-ui/react";
 
 export default function TableIndexPage() {
   return (
     <Card>
       <CardBody>
-        <Text>View a summary of all your customers over the last month.</Text>
+        <Text>Table List</Text>
+        <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+          <GridItem w="100%" h="10" bg="blue.500" />
+          <GridItem w="100%" h="10" bg="blue.500" />
+          <GridItem w="100%" h="10" bg="blue.500" />
+          <GridItem w="100%" h="10" bg="blue.500" />
+          <GridItem w="100%" h="10" bg="blue.500" />
+        </Grid>
       </CardBody>
     </Card>);
 }
