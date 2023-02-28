724b0b21a0401ce30c66bba56b2f56f2b586ca7f
sivan wolberg <sivan@wolberg.pro>
2023-02-28 13:20:58 +0200

    chore: setup seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,25 @@
-import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcryptjs";
+import { PrismaClient, tableType } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 async function seed() {
 
+  await prisma.tables.createMany({
+    skipDuplicates: true,
+    data: [
+      { type: tableType.Bar, size: 1 },
+      { type: tableType.Bar, size: 1 },
+      { type: tableType.Bar, size: 1 },
+      { type: tableType.Bar, size: 1 },
+      { type: tableType.SmallTable, size: 4 },
+      { type: tableType.SmallTable, size: 4 },
+      { type: tableType.SmallTable, size: 4 },
+      { type: tableType.MedTable, size: 8 },
+      { type: tableType.MedTable, size: 8 },
+      { type: tableType.LargeTable, size: 12 },
+      { type: tableType.LargeTable, size: 12 }
+    ]
+  });
   console.log(`Database has been seeded. 🌱`);
 }
 
